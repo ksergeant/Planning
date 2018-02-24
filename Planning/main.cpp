ff6dd97cea19fa88feb8529b7bcc307d566f10e7
Magi <jamescaron37@yahoo.fr>
2018-02-24 02:09:32 +0100

    Formatage de la date

--- Planning/main.cpp
+++ Planning/main.cpp
@@ -15,6 +15,8 @@
 
 using namespace std;
 
+
+
 struct data { // structure data
 
     string vacation;
@@ -42,7 +44,9 @@ int main(int argc, const char * argv[]) {
     
     printf("Nous sommes %s,  ", NomJourSemaine[t->tm_wday]);
     printf("le %02u %s %04u,  ", t->tm_mday, NomMois[t->tm_mon], 1900 + t->tm_year); // départ le 10 novembre 2017
-    cout << "Start" <<endl<<endl;
+    cout << "Start" <<endl;
+    strftime(format, 128, "%Y/%m/%d\n", t);
+    cout << format <<endl;
     
     // Boucle qui calcule les jours sur les 10 prochaines années (84 cas possibles)
     
@@ -61,12 +65,10 @@ int main(int argc, const char * argv[]) {
         // switch des 84 possibilitées
         
         switch (compteurJourVac) {
+            
             case 1: cout << "Aprem" <<endl;
-                
-                
                 strftime(format, 128, "%Y/%m/%d\n", t); // format de date utilisable pour la base de données 
                 cout << format <<endl;
-                
                 break; //debut vac aprem
                 
             case 2: cout << "Aprem" <<endl;
@@ -74,31 +76,76 @@ int main(int argc, const char * argv[]) {
                 cout << format <<endl;
                 break;
                 
-            case 3: cout << "Aprem" <<endl; break;
-            case 4: cout << "Aprem" <<endl<<endl; break;
+            case 3: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
-            case 5: cout << "Repos" <<endl; break;
-            case 6: cout << "Repos" <<endl; break;
-            case 7: cout << "Repos" <<endl; break;
-            case 8: cout << "Repos" <<endl; break;
-            case 9: cout << "Repos" <<endl<<endl; break;
+            case 4: cout << "Aprem" <<endl<<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
-            case 10: cout << "Aprem" <<endl; break;
-            case 11: cout << "Aprem" <<endl; break;
-            case 12: cout << "Aprem" <<endl; break;
-            case 13: cout << "Aprem" <<endl; break;
-            case 14: cout << "Aprem" <<endl<<endl; break;
+            case 5: cout << "Repos" <<endl;break;
+            case 6: cout << "Repos" <<endl;break;
+            case 7: cout << "Repos" <<endl;break;
+            case 8: cout << "Repos" <<endl;break;
+            case 9: cout << "Repos" <<endl;break;
                 
-            case 15: cout << "Repos" <<endl; break;
-            case 16: cout << "Repos" <<endl; break;
-            case 17: cout << "Repos" <<endl; break;
+            case 10: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 11: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 12: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 13: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 14: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 15: cout << "Repos" <<endl;break;
+            case 16: cout << "Repos" <<endl;break;
+            case 17: cout << "Repos" <<endl;break;
             case 18: cout << "Repos" <<endl<<endl; break;
                 
-            case 19: cout << "Aprem" <<endl; break;
-            case 20: cout << "Aprem" <<endl; break;
-            case 21: cout << "Aprem" <<endl; break;
-            case 22: cout << "Aprem" <<endl; break;
-            case 23: cout << "Aprem" <<endl<<endl; break;
+            case 19: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 20: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 21: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 22: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 23: cout << "Aprem" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
             case 24: cout << "Repos" <<endl; break;
             case 25: cout << "Repos" <<endl; break;
@@ -106,10 +153,25 @@ int main(int argc, const char * argv[]) {
             case 27: cout << "Repos" <<endl; break;
             case 28: cout << "Repos" <<endl<<endl; break; //fin vac aprem
                 
-            case 29: cout << "Matin" <<endl; break; //debut vac matin
-            case 30: cout << "Matin" <<endl; break;
-            case 31: cout << "Matin" <<endl; break;
-            case 32: cout << "Matin" <<endl<<endl; break;
+            case 29: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break; //debut vac matin
+                
+            case 30: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 31: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 32: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
             case 33: cout << "Repos" <<endl; break;
             case 34: cout << "Repos" <<endl; break;
@@ -117,22 +179,60 @@ int main(int argc, const char * argv[]) {
             case 36: cout << "Repos" <<endl; break;
             case 37: cout << "Repos" <<endl<<endl; break;
                 
-            case 38: cout << "Matin" <<endl; break;
-            case 39: cout << "Matin" <<endl; break;
-            case 40: cout << "Matin" <<endl; break;
-            case 41: cout << "Matin" <<endl; break;
-            case 42: cout << "Matin" <<endl<<endl; break;
+            case 38: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 39: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 40: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 41: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 42: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
             case 43: cout << "Repos" <<endl; break;
             case 44: cout << "Repos" <<endl; break;
             case 45: cout << "Repos" <<endl; break;
             case 46: cout << "Repos" <<endl<<endl; break;
                 
-            case 47: cout << "Matin" <<endl; break;
-            case 48: cout << "Matin" <<endl; break;
-            case 49: cout << "Matin" <<endl; break;
-            case 50: cout << "Matin" <<endl; break;
-            case 51: cout << "Matin" <<endl<<endl; break;
+            case 47: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 48: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 49: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 50: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 51: cout << "Matin" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
             case 52: cout << "Repos" <<endl; break;
             case 53: cout << "Repos" <<endl; break;
@@ -140,10 +240,25 @@ int main(int argc, const char * argv[]) {
             case 55: cout << "Repos" <<endl; break;
             case 56: cout << "Repos" <<endl<<endl; break; //fin vac matin
                 
-            case 57: cout << "Nuit" <<endl; break; //debut vac nuit
-            case 58: cout << "Nuit" <<endl; break;
-            case 59: cout << "Nuit" <<endl; break;
-            case 60: cout << "Nuit" <<endl<<endl; break;
+            case 57: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break; //debut vac nuit
+                
+            case 58: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 59: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 60: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
             case 61: cout << "Repos" <<endl; break;
             case 62: cout << "Repos" <<endl; break;
@@ -151,22 +266,60 @@ int main(int argc, const char * argv[]) {
             case 64: cout << "Repos" <<endl; break;
             case 65: cout << "Repos" <<endl<<endl; break;
                 
-            case 66: cout << "Nuit" <<endl; break;
-            case 67: cout << "Nuit" <<endl; break;
-            case 68: cout << "Nuit" <<endl; break;
-            case 69: cout << "Nuit" <<endl; break;
-            case 70: cout << "Nuit" <<endl<<endl; break;
+            case 66: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 67: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 68: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 69: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+            case 70: cout << "Nuit" <<endl<<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
                 
             case 71: cout << "Repos" <<endl; break;
             case 72: cout << "Repos" <<endl; break;
             case 73: cout << "Repos" <<endl; break;
             case 74: cout << "Repos" <<endl<<endl; break;
                 
-            case 75: cout << "Nuit" <<endl; break;
-            case 76: cout << "Nuit" <<endl; break;
-            case 77: cout << "Nuit" <<endl; break;
-            case 78: cout << "Nuit" <<endl; break;
-            case 79: cout << "Nuit" <<endl<<endl; break;
+            case 75: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 76: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 77: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 78: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                break;
+                
+            case 79: cout << "Nuit" <<endl;
+                strftime(format, 128, "%Y/%m/%d\n", t);
+                cout << format <<endl;
+                
+                break;
                 
             case 80: cout << "Repos" <<endl; break;
             case 81: cout << "Repos" <<endl; break;
